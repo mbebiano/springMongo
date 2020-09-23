24f89b387a841e9f60a0aa0f5be0faa7a880a0af
mbebiano <mattheus.bebiano@engenharia.ufjf.br>
2020-09-23 15:52:27 -0300

    Refatoração de código padrão MVC Controladores Acessa a camada de serviço e o Serviço acessa o repositorio

--- springMongo/src/main/java/br/com/ntendencia/resources/UserResource.java
+++ springMongo/src/main/java/br/com/ntendencia/resources/UserResource.java
@@ -23,10 +23,7 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1", "Maria Brown", "maria@gmail.com");
-		User alex = new User("2", "Alex Green", "alex@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, alex));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
