819f63ae12f470c8f759e50184aa85b918fbc0eb
mbebiano <mattheus.bebiano@engenharia.ufjf.br>
2020-09-24 10:35:00 -0300

    feat: Usando Padrão DTO para retornar os usuários

--- springMongo/src/main/java/br/com/ntendencia/resources/UserResource.java
+++ springMongo/src/main/java/br/com/ntendencia/resources/UserResource.java
@@ -1,6 +1,7 @@
 package br.com.ntendencia.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ntendencia.domain.User;
+import br.com.ntendencia.dto.UserDTO;
 import br.com.ntendencia.services.UserService;
 
 //caminho do endpoint
@@ -20,9 +22,10 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
