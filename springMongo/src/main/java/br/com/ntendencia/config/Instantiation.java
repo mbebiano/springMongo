fab8d96325f4a5909ca0a62cf8d0c059d1de458e
mbebiano <mattheus.bebiano@engenharia.ufjf.br>
2020-09-29 09:12:52 -0300

    feat: Acrescentando comentários aos posts

--- springMongo/src/main/java/br/com/ntendencia/config/Instantiation.java
+++ springMongo/src/main/java/br/com/ntendencia/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import br.com.ntendencia.domain.Post;
 import br.com.ntendencia.domain.User;
 import br.com.ntendencia.dto.AuthorDTO;
+import br.com.ntendencia.dto.CommentDTO;
 import br.com.ntendencia.repository.PostRepository;
 import br.com.ntendencia.repository.UserRepository;
 
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018") , "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));		
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(alex));
+		
+		CommentDTO c1= new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		//salva os usuários para que adquiram id proprio userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
--- springMongo/src/main/java/br/com/ntendencia/domain/Post.java
+++ springMongo/src/main/java/br/com/ntendencia/domain/Post.java
@@ -1,12 +1,15 @@
 package br.com.ntendencia.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import br.com.ntendencia.dto.AuthorDTO;
+import br.com.ntendencia.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -19,16 +22,13 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
-	public String getId() {
-		return id;
-	}
+	
+	private List<CommentDTO> comments = new ArrayList<>();		
 	
 	public Post() {
 		
 	}
 	
-	
-
 	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
@@ -36,6 +36,10 @@ public class Post implements Serializable{
 		this.title = title;
 		this.body = body;
 		this.author = author;
+	}
+	
+	public String getId() {
+		return id;
 	}
 
 	public void setId(String id) {
@@ -67,6 +71,13 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 	
 	
 	@Override
