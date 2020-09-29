c9f33978d368482bbb0404fe60a9515022e75783
mbebiano <mattheus.bebiano@engenharia.ufjf.br>
2020-09-29 11:37:36 -0300

    feat: Consulta com vários critérios

--- springMongo/src/main/java/br/com/ntendencia/config/Instantiation.java
+++ springMongo/src/main/java/br/com/ntendencia/config/Instantiation.java
@@ -38,8 +38,8 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018") , "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));		
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(alex));
+		Post post1 = new Post(null, sdf.parse("21/03/2021") , "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));		
+		Post post2 = new Post(null, sdf.parse("23/03/2021"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(alex));
 		
 		CommentDTO c1= new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
 		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
